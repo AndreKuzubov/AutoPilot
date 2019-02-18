a49f1bbf404786939933e09f2f936f7cd51629bc
Andrey Kuzubov <smoll9521@gmail.com>
2019-02-18 21:38:30 +0300

    utils: internet, tensorSpecific utils, imageUtils

--- RoadSign/utils/imageProccessing.py
+++ RoadSign/utils/imageProccessing.py
@@ -8,13 +8,24 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 def wbImage(image):
     x = tf.constant(np.asarray(image), dtype=tf.float32)
-    y = tf.constant(np.asarray(
-        [
-            [1 / 3, 1 / 3, 1 / 3],
-            [1 / 3, 1 / 3, 1 / 3],
-            [1 / 3, 1 / 3, 1 / 3],
-        ]
-    ), dtype=tf.float32)
+
+    if (x.shape[2] == 4):
+        y = tf.constant(np.asarray(
+            [
+                [1 / 3, 1 / 3, 1 / 3, 0],
+                [1 / 3, 1 / 3, 1 / 3, 0],
+                [1 / 3, 1 / 3, 1 / 3, 0],
+                [0, 0, 0, 1],
+            ]
+        ), dtype=tf.float32)
+    else:
+        y = tf.constant(np.asarray(
+            [
+                [1 / 3, 1 / 3, 1 / 3],
+                [1 / 3, 1 / 3, 1 / 3],
+                [1 / 3, 1 / 3, 1 / 3],
+            ]
+        ), dtype=tf.float32)
 
     # print('x shape:', x.shape)
     # print('y shape:', y.shape)
@@ -24,6 +35,53 @@ def wbImage(image):
         sess.close()
         # print('result shape:', result.shape)
         return Image.fromarray(np.array(result, dtype=np.uint8))
+
+
+def lightingImage(image, ratio):
+    x = tf.constant(np.asarray(image), dtype=tf.float32)
+    y = tf.constant(np.asarray(
+        [ratio, ratio, ratio, 1],
+    ), dtype=tf.float32)
+
+    print('x shape:', x.shape)
+    print('y shape:', y.shape)
+    out = tf.einsum("ijk,k->ijk", x, y)
+    with tf.Session() as sess:
+        result = sess.run(out)
+        sess.close()
+        # print('result shape:', result.shape)
+        return Image.fromarray(np.array(result, dtype=np.uint8))
+
+
+def contrastImage(image, ratio):
+    x = tf.constant(np.asarray(image), dtype=tf.float32)
+    y = tf.constant(1 / 8 * np.asarray(
+        [
+            [1, 1, 1],
+            [1, 2, 1],
+            [1, 1, 1]
+        ]
+    ), dtype=tf.float32)
+
+    print('x shape:', x.shape)
+    print('y shape:', y.shape)
+
+    out = tf.nn.conv2d(x, y, padding="VALID", strides=[1, 1, 1, 1])
+    with tf.Session() as sess:
+        result = sess.run(out)
+        sess.close()
+        # print('result shape:', result.shape)
+        return Image.fromarray(np.array(result, dtype=np.uint8))
+
+
+def blur(sourceFile, destinationFile, radius=100, sigma=1):
+    os.system("magick convert -blur {radius}x{sigma} {source} {destination}"
+        .format(
+        source=sourceFile,
+        destination=destinationFile,
+        radius=str(radius),
+        sigma=str(sigma)
+    ))
 
 
 def glueImagesHorisontal(images, size=(30, 30)):
