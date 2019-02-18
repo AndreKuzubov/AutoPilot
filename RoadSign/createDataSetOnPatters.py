0be2304eed46bcb6ba6dfc91f4909350d52eb987
Andrey Kuzubov <smoll9521@gmail.com>
2019-02-18 21:40:03 +0300

    loading dorZnaki and GTSRB

--- RoadSign/createDataSetOnPatters.py
+++ RoadSign/createDataSetOnPatters.py
@@ -1,6 +1,9 @@
 import os
 import shutil
+import sys
 
+from RoadSign.utils.specificFixs import *
+from RoadSign.utils.inetUtils import *
 import requests
 import lxml.html as lxmlHtml
 from urllib.parse import urljoin
@@ -12,14 +15,47 @@ import glob
 from RoadSign.utils import imageProccessing
 from RoadSign.utils import utils
 
-
 #
 # Загрузка шаблонов с сайта
 #
 #
 #  @Andreykuzubov 2018
 
-def loadPatternsFromSite():
+IMAGE_SIZE = 512
+BASE_FOLDER = os.getcwd()
+
+GTSRB_DIR = "datasets/GTSRB/"
+DORZNAKI_DIR = "datasets/DorZnaki/"
+
+def loadGTSRB():
+    if (not os.path.exists(GTSRB_DIR+"GTSRB_Final_Training_Images")):
+        print("loading GTSRB_Final_Training_Images...")
+        download_file("http://benchmark.ini.rub.de/Datasets/GTSRB_Final_Training_Images.zip",
+                      local_filename=GTSRB_DIR+"GTSRB_Final_Training_Images.zip")
+
+        print("unzipping GTSRB_Final_Training_Images...")
+        zip_ref = zipfile.ZipFile(GTSRB_DIR+"GTSRB_Final_Training_Images.zip", 'r')
+        zip_ref.extractall(GTSRB_DIR+"GTSRB_Final_Training_Images/")
+        zip_ref.close()
+        os.remove(GTSRB_DIR+"GTSRB_Final_Training_Images.zip")
+
+        print("success GTSRB_Final_Training_Images")
+
+    if (not os.path.exists(GTSRB_DIR+"GTSRB_Final_Test_Images")):
+        print("loading GTSRB_Final_Test_Images...")
+        download_file("http://benchmark.ini.rub.de/Datasets/GTSRB_Final_Test_Images.zip",
+                      local_filename=GTSRB_DIR+"GTSRB_Final_Test_Images.zip")
+
+        print("unzipping GTSRB_Final_Test_Images...")
+        zip_ref = zipfile.ZipFile(GTSRB_DIR+"GTSRB_Final_Test_Images.zip", 'r')
+        zip_ref.extractall(GTSRB_DIR+"GTSRB_Final_Test_Images/")
+        zip_ref.close()
+        os.remove(GTSRB_DIR+"GTSRB_Final_Test_Images.zip")
+
+        print("success GTSRB_Final_Test_Images")
+
+
+def loadDorZnaki():
     baseUrl = "http://www.artpatch.ru/dorznaki.html"
     html = requests.get(baseUrl).text.replace("<br>", '\n <br>')
     s = lxmlHtml.fromstring(html)
@@ -27,24 +63,23 @@ def loadPatternsFromSite():
     def loadToLocal(category, name, url):
         response = requests.get(url, stream=True)
         response.raise_for_status()
-        utils.createNoExistsFolders("RoadSign/datasets/patterns/{category}".format(category=category, name=name))
-        with open("RoadSign/datasets/patterns/{category}/{name}.zip".format(category=category, name=name),
+        utils.createNoExistsFolders(DORZNAKI_DIR+"{category}".format(category=category, name=name))
+        with open(DORZNAKI_DIR+"{category}/{name}.zip".format(category=category, name=name),
                   'wb') as handle:
             for block in response.iter_content(1024):
                 handle.write(block)
 
-        zip_ref = zipfile.ZipFile(
-            "RoadSign/datasets/patterns/{category}/{name}.zip".format(category=category, name=name), 'r')
+        zip_ref = zipfile.ZipFile(DORZNAKI_DIR+"{category}/{name}.zip".format(category=category, name=name), 'r')
 
-        utils.createNoExistsFolders("RoadSign/datasets/patterns/{category}/zip".format(category=category, name=name))
-        zip_ref.extractall("RoadSign/datasets/patterns/{category}/zip".format(category=category, name=name))
+        utils.createNoExistsFolders(DORZNAKI_DIR+"{category}/zip".format(category=category, name=name))
+        zip_ref.extractall(DORZNAKI_DIR+"{category}/zip".format(category=category, name=name))
         zip_ref.close()
 
-        filename = glob.glob("RoadSign/datasets/patterns/{category}/zip/*".format(category=category))[0]
-        os.rename(filename, "RoadSign/datasets/patterns/{category}/{name}.ai".format(category=category, name=name))
+        filename = glob.glob(DORZNAKI_DIR+"{category}/zip/*".format(category=category))[0]
+        os.rename(filename, DORZNAKI_DIR+"{category}/{name}.ai".format(category=category, name=name))
 
-        shutil.rmtree("RoadSign/datasets/patterns/{category}/zip".format(category=category))
-        os.remove("RoadSign/datasets/patterns/{category}/{name}.zip".format(category=category, name=name))
+        shutil.rmtree(DORZNAKI_DIR+"{category}/zip".format(category=category))
+        os.remove(DORZNAKI_DIR+"{category}/{name}.zip".format(category=category, name=name))
         print("loaded {name}".format(name=name))
 
     content = s.xpath('//*[@id="content"]')[0]
@@ -53,41 +88,38 @@ def loadPatternsFromSite():
     for child in content.getchildren()[7:]:
         if (child.tag.lower() == "h2"):
             category = child.text
+            if category is None:
+                continue
+            category = category.replace(" ", "_")
         elif (child.tag.lower() == "br"):
             continue
         elif (list(child.classes)[0] == 'znSootLeft'):
             url = urljoin(base=baseUrl, url=child.getchildren()[0].attrib['href'])
         elif (list(child.classes)[0] == 'znSootRight1'):
             name = child.text_content().replace('\n', '')
-            loadToLocal(category=category, url=url, name=name)
+            name = name.replace(' ', '_')
+            if not os.path.exists(
+                    DORZNAKI_DIR+"{category}/{name}.ai".format(category=category, name=name)):
+                loadToLocal(category=category, url=url, name=name)
 
-
-def createDataSetFromPatterns():
-    bash_script = """magick convert ai:'{basefolder}/{filesource}'  -resize '{size}x' -density {density} '{basefolder}/{filedestination}'"""
-
-    baseFolder = os.getcwd()
-    paternsFolder = "RoadSign/datasets/patterns"
-    densityFolder = "RoadSign/datasets/fromPattern_original"
-    for category in next(os.walk(paternsFolder))[1]:
-        for fileName in next(os.walk(paternsFolder + "/" + category))[2]:
-            fileSource = paternsFolder + '/' + category + '/' + fileName
-
-            utils.createNoExistsFolders(densityFolder + '/' + category + '/' + fileName[:-3])
-            for density in range(100, 500, 50):
-                for size in range(50, 500, 50):
-                    fileDesitination = densityFolder + '/' + category + '/' + fileName[:-3] + '/' \
-                                       + str(density) + "_" + str(size) + '.png'
-                    os.system(
-                        bash_script.format(
-                            basefolder=baseFolder,
-                            filesource=fileSource,
-                            filedestination=fileDesitination,
-                            size=str(size),
-                            density=str(density))
-                    )
-            print("files created: " + fileName)
+            if not os.path.exists(
+                    DORZNAKI_DIR+"{category}/{name}.png".format(category=category, name=name)):
+                bash_script = """magick convert ai:'{basefolder}/{filesource}'  -resize '{size}x' -density {density} '{basefolder}/{filedestination}'"""
+                os.system(
+                    bash_script.format(
+                        basefolder=BASE_FOLDER,
+                        filesource=DORZNAKI_DIR+"{category}/{name}.ai".format(category=category,
+                                                                                   name=name),
+                        filedestination=DORZNAKI_DIR+"{category}/{name}.png".format(category=category,
+                                                                                         name=name),
+                        size=str(IMAGE_SIZE),
+                        density=str(100))
+                )
+                print("files created: " + "{name}.png".format(category=category, name=name))
 
 
 if __name__ == "__main__":
-    # loadPatternsFromSite()
-    createDataSetFromPatterns()
+    loadGTSRB()
+
+    loadDorZnaki()
+
